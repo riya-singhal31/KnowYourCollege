2e273f144d71b75083b5c0888a4f1061410712a8
riya-singhal31 <65610730+riya-singhal31@users.noreply.github.com>
2021-06-13 23:29:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+showNotes();
+
+// If user adds a note, add it to the localStorage
+let addBtn = document.getElementById("addBtn");
+addBtn.addEventListener("click", function(e) {
+  let addTxt = document.getElementById("addTxt");
+
+
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  notesObj.push(addTxt.value);
+  //notesObj.push();
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  addTxt.value = "";
+
+  showNotes();
+});
+
+// Function to show elements from localStorage
+function showNotes() {
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  let html = "";
+
+  notesObj.forEach(function(element, index) {
+    if(element)
+    html += `
+            <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Anonymous</h5>
+
+                        <p class="card-text"> ${element}</p>
+                        <button id="${index}"onclick="editReview(this.id)" class="btn btn-primary">Edit</button>
+
+                    </div>
+                </div>`;
+  });
+  let notesElm = document.getElementById("notes");
+  if (notesObj.length != 0) {
+    notesElm.innerHTML = html;
+  } else {
+    notesElm.innerHTML = `Nothing to show! Use "Add Review" section above to add reviews.`;
+
+  }
+}
+
+function editReview(index){
+  index.innerHTML= "";
+}
+
+
+let search = document.getElementById('searchTxt');
+search.addEventListener("input", function(){
+
+    let inputVal = search.value.toLowerCase();
+
+    let noteCards = document.getElementsByClassName('noteCard');
+    Array.from(noteCards).forEach(function(element){
+        let cardTxt = element.getElementsByTagName("p")[0].innerText;
+        if(cardTxt.includes(inputVal)){
+            element.style.display = "block";
+        }
+        else{
+            element.style.display = "none";
+        }
+
+    })
+})
+//localStorage.clear();
